d907d11b224abb2dc69aee122b997b144b5c074c
LeonardoLB <leonardolsbufalo@gmail.com>
2019-06-27 15:16:15 -0300

    doGet

---
+++ src/main/java/ContatoServelet.java
@@ -0,0 +1,15 @@
+package main.java;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ContatoServelet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
